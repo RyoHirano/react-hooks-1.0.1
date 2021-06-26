c02b3da3a70445d20b19075e3ffb8df597de0aed
Ryo Hirano <ryo.hirano@monocrea.co.jp>
2021-06-26 12:23:24 +0900

    feat:implement component using multiple.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,29 @@
 import React, {useState} from "react";
 
-const App = () => {
-  const [count, setCount] = useState(0)
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count - 1)
+const App = props => {
 
-  const increment2 = () => setCount(previosCount => previosCount + 1)
-  const decrement2 = () => setCount(previosCount => previosCount - 1)
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
 
-  const reset = () => setCount(0)
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
 
-  const multiplication = () => setCount(count * 2)
-
-  const threeQuotient = () => count % 3 === 0 ? setCount(count / 3) : count
   return (
     <>
-    <div>
-     count: {count}
-    </div>
-    <div>
-     <button onClick={increment}>+1</button>
-     <button onClick={decrement}>-1</button>
-    </div>
-    <div>
-      <button onClick={increment2}>+1</button>
-      <button onClick={decrement2}>-1</button>
-    </div>
-    <div>
-      <button onClick={reset}>reset</button>
-      <button onClick={multiplication}>x2</button>
-      <button onClick={threeQuotient}>3の倍数の時だけ3で割る</button>
-    </div>
+      <p>現在の{name}は、{price}円です。</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={name} onChange={e => setName(e.target.value)}/>
     </>
   )
+}
+
+App.defaultProps = {
+  name : '',
+  price : 1000
 }
 
 export default App;
