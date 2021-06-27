6ec5cda8d862f0e883b86788bab2070d4c0712ba
Ryo Hirano <ryo.hirano@monocrea.co.jp>
2021-06-27 11:35:27 +0900

    feat:create operation logs.

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,8 @@ import AppContext from '../contexts/AppContext'
 
 const App = () => {
   const insitalState = {
-    events: []
+    events: [],
+    operationLogs: [],
   }
   const [state, dispatch] = useReducer(reducer, insitalState)
 
