0b8ed27720b8c009da158b4f851b15b59c414c8c
Ryo Hirano <ryo.hirano@monocrea.co.jp>
2021-06-27 12:17:59 +0900

    feat:delete operation logs.

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -47,6 +47,14 @@ const EventForm = () => {
     }
   }
 
+  const deleteAllOperationLogs = e => {
+    e.preventDefault();
+    const result = window.confirm(
+      '全ての操作ログを本当に削除しても良いですか？'
+    )
+    result && dispatch({type: DELETE_ALL_OPERATION_LOGS})
+  }
+
   const unCreatable = title === '' || body === ''
   return (
     <>
@@ -85,6 +93,13 @@ const EventForm = () => {
           disabled={!state.events.length}
         >
           全てのイベントを削除する
+        </button>
+        <button
+          className="btn btn-danger"
+          onClick={deleteAllOperationLogs}
+          disabled={!state.operationLogs.length}
+        >
+          全ての操作ログを削除する
         </button>
       </form>
     </>
